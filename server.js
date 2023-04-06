7e10a224d1c4e647cde9b6d922d7d7dc1b1762df
dagmara <dagmara.gabriela.stach@gmail.com>
2023-04-06 21:54:55 +0200

    res.json body "succes"

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors())
 
 database = {
     users: [
@@ -23,6 +26,12 @@ database = {
             entries: 0,
             joined: new Date()
         },
+    ], login:[
+        {
+            id: '199',
+            hash: '',
+            email: "gaga376@wp.pl"
+        }
     ]
 }
 
@@ -31,9 +40,16 @@ app.get("/", (req, res) => {
 })
 
 app.post("/signin", (req, res) => {
+    bcrypt.compare("bacon", "$2a$10$FiFHnsAwS7odQrrwNZbTie285Qt4Y0t3iIfEFTT.uJrE1OhvfD9fO", function(err, res) {
+ console.log("first guess", res);
+    });
+    bcrypt.compare("veggies", "$2a$10$FiFHnsAwS7odQrrwNZbTie285Qt4Y0t3iIfEFTT.uJrE1OhvfD9fO", function(err, res) {
+        console.log("second guess", res);
+    });
+
     if (req.body.email === database.users[0].email
         && req.body.password === database.users[0].password) {
-        res.json("juupi")
+        res.json("succes")
     } else {
         res.status(400).json("upps no working")
     }
@@ -41,6 +57,11 @@ app.post("/signin", (req, res) => {
 
 app.post("/register", (req, res) => {
     const {email, name, password} = req.body
+
+    bcrypt.hash(password, null, null, function(err, hash) {
+       console.log(hash)
+    });
+
     database.users.push({
         id: "123",
         name: name,
@@ -80,6 +101,14 @@ app.post("/image", (req,res)=>{
         res.status(404).json("not found");
     }
 });
+
+bcrypt.hash("bacon", null, null, function(err, hash) {
+    // Store hash in your password DB.
+});
+
+
+
+
 
 app.listen(3000, () => {
     console.log('app is running')

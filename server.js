3d4cb11cd82a77e3f8c1354a22cf444a3783bdc0
dagmara <dagmara.gabriela.stach@gmail.com>
2023-04-28 12:03:16 +0200

    app.post("/register"  trx transaction post a new user into (db.user), password hash generate, trx transaction post user data(email, hash, id) to login table,
    bcrypt.hashSync(password)
    trx.insert
    .into
    .returning
    .insert
    .then(trx.commit)
    .catch(trx.rollback)

--- server.js
+++ server.js
@@ -74,14 +74,43 @@ app.post("/signin", (req, res) => {
 app.post("/register", (req, res) => {
     const {email, name, password} = req.body;
 
-    db('users')
-        .returning('*')
-        .insert({
-        email: email,
-        name: name,
-        joined: new Date()
-    }).then(user => {
-        res.json(user[0]);
+    const hash = bcrypt.hashSync(password);
+    // create db.transaction if we have more than ones things
+   // use this trx object instead of db to do this operations
+    // insert data
+    // into it login table
+    // return the email
+    // use a loginEmail to return another transaction
+    // and to insert into it users
+    // email - instead loginEmail, email instead of loginEmail[0].email,
+    // also works
+    //
+    //then respond with a json()
+    // if we want to add it to sql
+    // need to trx transaction commit
+    // if anything fails - function catch will rollback the changes
+
+
+    db.transaction(trx => {
+        trx.insert({
+            hash: hash,
+            email: email
+        })
+            .into('login')
+            .returning('email')
+            .then(loginEmail =>{
+                return trx('users')
+                    .returning('*')
+                    .insert({
+                        email: loginEmail[0].email,
+                        name: name,
+                        joined: new Date()
+                    }).then(user => {
+                        res.json(user[0]);
+                    })
+            })
+            .then(trx.commit) // trx.commit - put into it this data
+            .catch(trx.rollback) // if anything change - rollback the changes
     })
         .catch(err => res.status(400).json("unable to register"))
 })

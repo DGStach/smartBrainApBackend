fa6de1c8aca0bbb8253cde7a137935816c5e7f02
dagmara <dagmara.gabriela.stach@gmail.com>
2023-05-05 14:31:54 +0200

    refactore it

--- controllers/register.js
+++ controllers/register.js
@@ -19,6 +19,9 @@ const handleRegister = (db ,bcrypt)=>
     // if anything fails - function catch will rollback the changes
 
     const {email, name, password} = req.body;
+    if (!email || !name || !password){
+        return res.status(400).json('incorrect form submission')
+    }
     const hash = bcrypt.hashSync(password);
     db.transaction(trx => {
         trx.insert({

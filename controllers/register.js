254e800196d07a910494240a5fc66b91d92e1d84
dagmara <dagmara.gabriela.stach@gmail.com>
2023-05-16 11:50:50 +0200

    works

--- controllers/register.js
+++ controllers/register.js
@@ -2,29 +2,11 @@ const bcrypt = require("bcrypt-nodejs");
 
 const handleRegister = (db ,bcrypt)=>
 (req, res) => {
-    // create db.transaction if we have more than ones things
-    // use this trx object instead of db to do this operations
-    // insert data
-    // into it login table
-    // return the email
-    // use a loginEmail to return another transaction
-    // and to insert into it users
-    // email - instead loginEmail, email instead of loginEmail[0].email,
-    // also works
-    //then respond with a json()
-    // if we want to add it to sql
-    // need to trx transaction commit
-    // if anything fails - function catch will rollback the changes
-
     const {email, name, password} = req.body;
     console.log('req.body', req.body.email);
- /*   console.log("name", name)
-    console.log("email", email)
-    console.log("password", password)*/
-
-/*    if (!email || !name || !password){
+   if (!email || !name || !password){
         return res.status(400).json('incorrect form submission')
-    }*/
+    }
     console.log('email', email);
     const hash = bcrypt.hashSync(password);
     console.log('hash', hash);
@@ -47,8 +29,8 @@ const handleRegister = (db ,bcrypt)=>
                        res.json(user[0])
                     })
             })
-            .then(trx.commit) // trx.commit - put into it this data
-            .catch(trx.rollback) // if anything change - rollback the changes
+            .then(trx.commit)
+            .catch(trx.rollback)
     })
         .catch(err => res.status(400).json(err))
 }
